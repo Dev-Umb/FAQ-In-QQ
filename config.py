33593ccbb4f0bd886368eb49c9967093048c546c
farewell12345 <1149558764@qq.com>
2020-08-22 16:08:50 +0800

    修复数据

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BlackGroup = []
 #群组黑名单，可以利用这个来对bot的功能进行开关（本插件暂不支持，需要您手动写一个）
 HOST='0.0.0.0'#本地地址
 PORT=80#端口
-BOTQQ=3394886607#Bot的QQ，一定要写，否则会报找不到无头客户端的error
+BOTQQ=123456#Bot的QQ，一定要写，否则会报找不到无头客户端的error
 API_ROOT='http://localhost:80'#本地地址
 
 '''下面是bot的初始化'''
